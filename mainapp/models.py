82bdadea15ee064f59f87f4c990e8a5fd27eb5e4
majahed228 <pluzhnikov93@inbox.ru>
2022-09-25 23:50:55 +0300

    added author model/serializer/view

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Author(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid4)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday = models.PositiveIntegerField()
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Author
+from .serializers import AuthorModelSerializer
 
-# Create your views here.
+
+class AuthorViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
