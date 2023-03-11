6c6367ee539411c1b9e4c27495851321b4f5dd43
user <pluzhnikov93@inbox.ru>
2023-03-12 00:09:48 +0300

    added GraphAL scheme

--- library/schema.py
+++ library/schema.py
@@ -3,6 +3,7 @@ from graphene_django import DjangoObjectType
 from mainapp.models import Book, Author
 
 
+#DjangoObjectType позволяет автоматически создавать схемы на основе полей в БД
 class BookType(DjangoObjectType):
     class Meta:
         model = Book
@@ -18,6 +19,8 @@ class AuthorType(DjangoObjectType):
 class Query(graphene.ObjectType):
     all_books = graphene.List(BookType)
     all_authors = graphene.List(AuthorType)
+    author_by_id = graphene.Field(AuthorType, id=graphene.Int(required=True))
+    books_by_author_name = graphene.List(BookType, name=graphene.String(required=False))
 
     def resolve_all_books(self, info):
         return Book.objects.all()
@@ -25,7 +28,41 @@ class Query(graphene.ObjectType):
     def resolve_all_authors(self, info):
         return Author.objects.all()
 
+    def resolve_author_by_id(self, id):
+        try:
+            return Author.objects.get(pk=id)
+        except Author.DoesNotExist:
+            return None
 
-schema = graphene.Schema(query=Query)
+    def resolve_book_by_author_name(self, info, name=None):
+        books = Book.objects.all()
+        if name:
+            books = books.filter(author__first_name=name)
+        return books
+
+
+class AuthorMutation(graphene.Mutation):
+    class Arguments:
+        first_name = graphene.String()
+        last_name = graphene.String()
+        birthday_year = graphene.Int()
+
+    author = graphene.Field(AuthorType)
+
+    @classmethod
+    def mutate(cls, root, info, first_name, last_name, birthday_year):
+        author = Author.objects.get(pk=id)
+        author.first_name = first_name
+        author.last_name = last_name
+        author.birthday_year = birthday_year
+        author.save()
+        return AuthorMutation(author=author)
+
+
+class Mutation(graphene.ObjectType):
+    update_author = AuthorMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 
